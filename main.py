004a2fa8a22d9e9f7b0b4d7dcc2b83a87bc2dda6
Bandoxa <kolosinsky@mail.ru>
2026-05-14 13:16:44 +0400

    Update main.py

--- main.py
+++ main.py
@@ -152,6 +152,6 @@ def callback():
     return "ok"
 
 # ЗАПУСК
-if name == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get('PORT', 10000))
     app.run(host="0.0.0.0", port=port)
